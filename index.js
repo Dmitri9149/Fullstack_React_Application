fd2fd3aa659881e32cec71649ee6acb86baf4afa
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-08 18:26:27 +0200

    bugs fixed, now all is working in delete

--- index.js
+++ index.js
@@ -61,12 +61,11 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
-app.delete('/api/persons/:id', (req,res) => {
-  const id = Number(req.params.id)
-  console.log("In delete!!!!!!!!!")
-  const persons = persons.filter(x => x.id !== id)
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
 
-  res.status(204).end()
+  response.status(204).end()
 })
 
 const PORT = 3001
