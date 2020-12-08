242a5c97fe121e257a303a12f95318204522cc7b
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-08 16:39:40 +0200

    delete method is added

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req,res)=> {
+  const id = Number(req.params.id)
+  const person = person.person.find(x => x.id != id )
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
