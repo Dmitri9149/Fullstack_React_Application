9f34682541c7bcd4801462173eb964df0c2655c2
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-11 14:00:19 +0200

    cors in use

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 
@@ -9,6 +10,8 @@ app.use(bodyParser.json())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
+
+app-use(cors())
 
 let persons = [
   { 
