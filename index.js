2bc94f4800da54282087c640fd28f808867801fd
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-15 13:23:24 +0200

    move code to models

--- index.js
+++ index.js
@@ -45,27 +45,6 @@ let persons = [
   }      
 ]
 
-const mongoose = require('mongoose')
-
-const url=process.env.MONGODB_URI
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number:String
-})
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Person = mongoose.model('Person', personSchema)
-
 const getRandomInt = () => 
   Math.floor(Math.random() * Math.floor(maxId))
 const maxId = 1000000000
