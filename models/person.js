00835afd63d79aaa84e338ab7e4ec2527fed48d6
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-15 14:19:23 +0200

    some changes

--- models/person.js
+++ models/person.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose')
 
 const url=process.env.MONGODB_URI
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+console.log('connecting.to', url)
+
+mongoose.connect(url, 
+  { useNewUrlParser: true, 
+    useUnifiedTopology: true, useFindAndModify: false, 
+    useCreateIndex: true })
+    .then(result => {
+      console.log('connected to MongoDB')
+  })
+  .catch((error)=> {
+      console.log('error connecting to DB: ', error.message)
+  })
+
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -18,8 +30,5 @@ personSchema.set('toJSON', {
 })
 
 const Person = mongoose.model('Person', personSchema)
-
-
-
 
 module.exports = mongoose.model('Person', personSchema)
