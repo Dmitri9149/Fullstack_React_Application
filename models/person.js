d8f842fa111804325e6e2294cd7a7f61fd837b33
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-23 21:26:42 +0200

    eslint errors are fixed

--- models/person.js
+++ models/person.js
@@ -1,40 +1,42 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
+// eslint-disable-next-line no-undef
 const url=process.env.MONGODB_URI
 
 console.log('connecting.to', url)
 
 mongoose.connect(url, 
-  { useNewUrlParser: true, 
-    useUnifiedTopology: true, useFindAndModify: false, 
-    useCreateIndex: true })
+    { useNewUrlParser: true, 
+        useUnifiedTopology: true, useFindAndModify: false, 
+        useCreateIndex: true })
+    // eslint-disable-next-line no-unused-vars
     .then(result => {
-      console.log('connected to MongoDB')
-  })
-  .catch((error)=> {
-      console.log('error connecting to DB: ', error.message)
-  })
+        console.log('connected to MongoDB')
+    })
+    .catch((error)=> {
+        console.log('error connecting to DB: ', error.message)
+    })
 
 
-  const personSchema =  new mongoose.Schema({
+const personSchema =  new mongoose.Schema({
     name: {
-      type:String,
-      minlength: 3,
-      required: true,
-      unique:true
+        type:String,
+        minlength: 3,
+        required: true,
+        unique:true
     },
     number: {
-      type:String,
-      minlength: 8
+        type:String,
+        minlength: 8
     }
-  })
+})
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 personSchema.plugin(uniqueValidator)
